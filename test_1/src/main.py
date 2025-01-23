cd8ea334a65a4025434fbea878a35b2fb41ee19e
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2025-01-23 21:08:08 +0000

    define function in test1

--- test_1/src/main.py
+++ test_1/src/main.py
@@ -1,9 +1,19 @@
-user_name = input("Hi Dear, May I know your name\n")
+# suggest a band name using city and pet names
+def suggest_band():
+    user_name = input("Hi Dear, May I know your name\n")
+    user_city = input(user_name + ", what City are you from?\n")
+    user_pet = input(user_name + ", please tell me the name of your pet\n")
+    user_brand = "The_"+user_city +"_"+ user_pet+"s"
+    print("Hahaaaa!!!\n Your brand is " + user_brand)
 
-user_city = input(user_name + ", what City are you from?\n")
+# finding the last character of a string
+def find_last_char():
+    word = input("Enter the word\n: ")
+    length = len(word)
+    last_char_index = length - 1
+    print("The last char of "+word +" is " + word[last_char_index])
 
-user_pet = input(user_name + ", please tell me the name of your pet\n")
 
-user_brand = "The_"+user_city +"_"+ user_pet+"s"
+find_last_char()
 
-print("Hahaaaa!!!\n Your brand is " + user_brand)
+
